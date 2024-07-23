8071aaac1640d5dc52e7319bfdedfd9417b7496f
Mohd Arman <armansheakh987@gmail.com>
2024-07-23 23:08:31 +0530

    updated Photo

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -5,7 +5,8 @@ import LinkedIn from "../../img/linkedin.png";
 import Instagram from "../../img/instagram.png";
 import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
-import boy from "../../img/boy.png";
+// import boy from "../../img/boy.png";
+import Pic from "../../img/newPic.png"
 import thumbup from "../../img/thumbup.png";
 import crown from "../../img/crown.png";
 import glassesimoji from "../../img/glassesimoji.png";
@@ -54,7 +55,7 @@ const Intro = () => {
       <div className="i-right">
         <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
-        <img src={boy} alt="" />
+        <img src={Pic} alt="" />
         <motion.img
           initial={{ left: "-36%" }}
           whileInView={{ left: "-24%" }}
